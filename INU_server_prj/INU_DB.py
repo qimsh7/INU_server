81ca74e28df46a8c12f44f0d2b62c3bda793053c
lcimsh <qimsh7@gmail.com>
2023-02-17 20:54:17 +0900

    MYSQL 연동

---
+++ INU_server_prj/INU_DB.py
@@ -0,0 +1,10 @@
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'INU_DB',
+        'USER': 'root',
+        'PASSWORD': 'girlf1026',
+        'HOST': 'localhost',
+        'PORT': '3306',
+    }
+}
